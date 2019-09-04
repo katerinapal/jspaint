e4af21f63fd223ab4518b0fb400c0d587f860095
katerina <katpaltoglou@gmail.com>
2019-09-04 17:55:33 +0300

    update webpack config file (bundle all modules to a module)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,16 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-	  index : './index.js'
+	  index : ['lib/jquery.min.js', 'lib/pep.js',
+			   'lib/canvas.toBlob.js', 'lib/gif.js/gif.js', 'lib/palette.js',
+			   'lib/FileSaver.js', 'lib/font-detective.js', 'src/helpers.js',
+			   'src/storage.js', 'src/$Component.js', 'src/$Window.js',
+			   'src/$ToolBox.js', 'src/$ColorBox.js', 'src/$FontBox.js',
+			   'src/$Handles.js', 'src/OnCanvasObject.js', 'src/Selection.js',
+			   'src/TextBox.js', 'src/image-manipulation.js', 'src/tool-options.js',
+			   'src/tools.js', 'src/functions.js', 'src/manage-storage.js',
+			   'src/help.js', 'src/menus.js', 'src/app.js', 'src/$menus.js',
+			   'src/canvas-change.js', 'src/sessions.js', './index.js']
   },
   output: {
       publicPath: '/',
